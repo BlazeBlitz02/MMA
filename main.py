e211b4e2d2d57c3b2224cb406c366b7973e2c562
Manh Tan Nguyen <nguyenmanhtan@student.tudelft.nl>
2022-03-21 20:36:37 +0100

    add main.py file

---
+++ main.py
@@ -0,0 +1,6 @@
+# call this file given the video path as argument and return a list of landmarks
+# input: video path
+# output: list of landmarks (can be >= 1 landmarks)
+# Note: This file is for main computation pipeline, data pipeline is (should be) handled in a
+# different file
+
