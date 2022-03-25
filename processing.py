05026f790b5b291212cbdd9cb9108551f412bbda
Shashwat <S.Sahay-1@student.tudelft.nl>
2022-03-25 16:53:20 +0100

    Fixed window function

--- processing.py
+++ processing.py
@@ -4,6 +4,7 @@
 
 import cv2
 import numpy as np
+from os import listdir
 
 
 def show_image(image, title="debug"):
@@ -17,23 +18,23 @@ def adjust_frame(image):
     # (width, height)
     new_size = (int(image.shape[1] / 4), int(image.shape[0] / 4))
     resized_image = cv2.resize(image, new_size)
-    show_image(resized_image)
+    # show_image(resized_image)
 
     # convert to grayscale
     gray = cv2.cvtColor(resized_image, cv2.COLOR_BGR2GRAY)
-    show_image(gray)
+    # show_image(gray)
 
     # blur image
     blurred_frame = cv2.GaussianBlur(gray, (5, 5), 0)
     return blurred_frame
 
 
-def frame_capture():
-    print("start")
+def frame_capture(file_path, sample_frequency):
+    print(file_path)
 
     # give this as arguments
-    file_path = "./invention_test_videos/nk_S_0.MOV"
-    sample_frequency = 1
+    # file_path = "./invention_test_videos/nk_S_0.MOV"
+    # sample_frequency = 1
 
     # open the video
     video = cv2.VideoCapture(file_path)
@@ -58,33 +59,38 @@ def frame_capture():
     print("frame total", count)
     print("end")
 
+
 def window(video, sample_frequency, frame):
     total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
-    left = frame - (sample_frequency/2) if frame - (sample_frequency/2) > 0 else 0
-    right = frame + (sample_frequency/2) if frame + (sample_frequency/2) < total_frames - 1 else total_frames
+    left = frame - (sample_frequency // 3) if frame - (sample_frequency // 3) > 0 else 0
+    right = frame + (sample_frequency // 3) if frame + (sample_frequency // 3) < total_frames - 1 else total_frames
     curr_frame_number = left + 1
     lowest_movement = left
-    smallest = 99999999999
+    smallest = None
     video.set(1, left)
     success, curr_frame = video.read()
-    while curr_frame_number <= right:
+    while curr_frame_number < right:
         video.set(1, curr_frame_number)
-        last_frame = curr_frame
+        last_frame = np.copy(curr_frame)
         success, curr_frame = video.read()
         if success:
             temp_diff = temporal(last_frame, curr_frame)
-            if np.sum(temp_diff) < smallest:
+            if smallest is None or np.sum(temp_diff) < smallest:
                 lowest_movement = curr_frame_number
                 smallest = np.sum(temp_diff)
         else:
             break
-    
+
         curr_frame_number += 1
-        
+
     return lowest_movement
-            
+
 
 def temporal(frameL, frameC):
     return frameL - frameC
 
-frame_capture()
+
+directory = "./invention_test_videos/"
+
+for file in listdir(directory):
+    frame_capture(directory + file, 1)
